f5638466745708497098b87c8f122c7004d6ff13
efeersan <161042207+efeersan89@users.noreply.github.com>
2025-09-16 19:24:14 +0100

    Update Test2.java

--- Test2.java
+++ Test2.java
@@ -2,6 +2,6 @@ package project6.Banka;
 
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println("test 2 olusturuldu");
+        System.out.println("test 2 olusturuldu ve ardından düzeltildi");
     }
 }
